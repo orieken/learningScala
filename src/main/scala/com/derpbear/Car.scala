27d34e6a1eebcae9a04627a106de3b3c3e3099f7
Oscar Rieken <oscar@ionicsecurity.com>
2015-04-04 18:34:03 -0400

    added driving and distance driven specs and functionality

--- src/main/scala/com/derpbear/Car.scala
+++ src/main/scala/com/derpbear/Car.scala
@@ -1,6 +1,13 @@
 package com.derpbear
 
-class Car {
+class Car(val year: Int) {
+  private var milesDriven: Int = 0
+
+  def miles = milesDriven
+
+  def drive(distance: Int) = {
+    milesDriven += Math.abs(distance)
+  }
 
   def turn(direction: String) = {
     "turning ... " + direction
--- src/test/scala/com/derpbear/CarSpec.scala
+++ src/test/scala/com/derpbear/CarSpec.scala
@@ -7,11 +7,16 @@ class CarSpec extends FunSpec with Matchers with BeforeAndAfterEach {
   var car: Car = _
 
   override def beforeEach(): Unit = {
-    car = new Car
+    car = new Car(2013)
   }
 
-
   describe("Car") {
+    describe("properties") {
+      it("was built in 2013") {
+        car.year should equal (2013)
+      }
+    }
+
     describe("Turning") {
       it("Turns left") {
         car turn "left" should equal ("turning ... left")
@@ -21,6 +26,17 @@ class CarSpec extends FunSpec with Matchers with BeforeAndAfterEach {
         car turn "right" should equal ("turning ... right")
       }
     }
-  }
 
+    describe("Driving") {
+      it("has 0 miles") {
+        val thisCar = new Car(2013)
+        thisCar.miles should equal (0)
+      }
+      it("has been driven 10 miles") {
+        val thisCar = new Car(2015)
+        thisCar.drive(10)
+        thisCar.miles should equal (10)
+      }
+    }
+  }
 }
