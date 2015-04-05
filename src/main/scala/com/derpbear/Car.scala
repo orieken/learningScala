8a934aa2d1766f4d94835b35601a8272b8d3191c
Oscar Rieken <oscar@ionicsecurity.com>
2015-04-04 23:08:20 -0400

    updated sensible typing examples

--- src/main/scala/com/derpbear/Car.scala
+++ src/main/scala/com/derpbear/Car.scala
@@ -14,3 +14,4 @@ class Car(val year: Int) {
   }
 
 }
+
--- src/test/scala/com/derpbear/SensibleTypingSpec.scala
+++ src/test/scala/com/derpbear/SensibleTypingSpec.scala
@@ -1,6 +1,5 @@
 package com.derpbear
 
-import java.util
 import java.util._
 
 import org.scalatest.{Matchers, FunSpec}
@@ -10,46 +9,45 @@ class SensibleTypingSpec extends FunSpec with Matchers {
     describe("defining an Int") {
       it("var year: Int") {
         var year: Int = 2009
-        year shouldBe an [Integer]
+        year shouldBe an[Integer]
       }
       it("var anotherYear = 2009") {
         var anotherYear = 2009
-        anotherYear shouldBe an [Integer]
+        anotherYear shouldBe an[Integer]
       }
     }
 
     describe("String") {
       it("var greet = 'Hello there'") {
         var greet = "Hello there"
-        greet shouldBe a [String]
+        greet shouldBe a[String]
       }
       it("var builder = new StringBuilder('hello')") {
         var builder = new StringBuilder("hello")
-        builder shouldBe a [StringBuilder]
+        builder shouldBe a[StringBuilder]
       }
     }
 
     describe("ArrayList") {
-      it("var list = Arraylist, is ArrayList[_]"){
+      it("var list = Arraylist, is ArrayList[_]") {
         var list = new ArrayList
-        list shouldBe an [ArrayList[_]]
+        list shouldBe an[ArrayList[_]]
       }
       it("ArrayList[Int] should not equal ArrayList[_]") {
         var list1 = new ArrayList[Int]
         var list2 = new ArrayList
 
-//      They should not compile because they are not the same types
+        //      They should not compile because they are not the same types
         "list2 = list1" shouldNot compile
       }
       it("ArrayList[Int] should not equal ArrayList[Any]") {
         var list1 = new ArrayList[Int]
         var list2 = new ArrayList[Any]
 
-//      They should not compile because they are not the same types
+        //      They should not compile because they are not the same types
         "list2 = list1" shouldNot compile
       }
     }
   }
-
-
 }
+
